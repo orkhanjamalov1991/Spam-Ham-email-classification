160c03e09cf488f9ce52d3128bdd80ab7e2dd7fc
orkhanjamalov1991 <orxancamalov1991@gmail.com>
2021-09-06 01:07:20 +0400

    Day 2.2 HW week12

---
+++ week12 day 2_2 hw.R
@@ -0,0 +1,136 @@
+#1. Import emails dataset and get familiarized with it.
+#2. Add `id` column defined by number of rows
+#3. Prepare data for fitting to the model
+#4. Use cv.glmnet for modeling
+#5. Give interpretation for train and test results
+
+library(data.table)
+library(tidyverse)
+library(text2vec)
+library(caTools)
+library(glmnet)
+
+emails
+emails %>% class()
+
+id <- rownames(emails)
+emails <- cbind(id=id, emails)
+
+emails %>% glimpse()
+
+emails$id <- emails$ID %>% as.character()
+set.seed(123)
+split <- emails$spam %>% sample.split(SplitRatio = 0.8)
+train <- emails %>% subset(split == T)
+test <- emails %>% subset(split == F)
+
+colnames(emails, do.NULL = TRUE, prefix = "col")
+
+it_train <- train$text %>% 
+  itoken(preprocessor = tolower, 
+         tokenizer = word_tokenizer,
+         ids = train$...1,
+         progressbar = F) 
+
+vocab <- it_train %>% create_vocabulary()
+vocab %>% 
+  arrange(desc(term_count)) %>% 
+  head(110) %>% 
+  tail(10) 
+
+vectorizer <- vocab %>% vocab_vectorizer()
+dtm_train <- it_train %>% create_dtm(vectorizer)
+
+dtm_train %>% dim()
+identical(rownames(dtm_train), train$id)
+
+
+glmnet_classifier <- dtm_train %>% 
+  cv.glmnet(y = train[['spam']],
+            family = 'binomial', 
+            type.measure = "auc",
+            nfolds = 10,
+            thresh = 0.001,# high value is less accurate, but has faster training
+            maxit = 1000)# again lower number of iterations for faster training
+
+glmnet_classifier$cvm %>% max() %>% round(3) %>% paste("-> Max AUC")
+
+
+it_test <- test$spam %>% tolower() %>% word_tokenizer()
+
+it_test <- it_test %>% 
+  itoken(ids = test$id,
+         progressbar = F)
+
+dtm_test <- it_test %>% create_dtm(vectorizer)
+
+dtm_test %>% head(1)
+
+preds <- predict(glmnet_classifier, dtm_test, type = 'response')[,1]
+glmnet:::auc(test$spam, preds) %>% round(2)
+
+
+stop_words <- c("i", "you", "he", "she", "it", "we", "they",
+                "me", "him", "her", "them",
+                "my", "your", "yours", "his", "our", "ours",
+                "myself", "yourself", "himself", "herself", "ourselves",
+                "the", "a", "an", "and", "or", "on", "by", "so",
+                "from", "about", "to", "for", "of", 
+                "that", "this", "is", "are")
+
+vocab <- it_train %>% create_vocabulary(stopwords = stop_words)
+
+
+pruned_vocab <- vocab %>% 
+  prune_vocabulary(term_count_min = 10, 
+                   doc_proportion_max = 0.5,
+                   doc_proportion_min = 0.001)
+pruned_vocab %>% 
+  arrange(desc(term_count)) %>% 
+  head(10) 
+
+vectorizer <- pruned_vocab %>% vocab_vectorizer()
+
+dtm_train <- it_train %>% create_dtm(vectorizer)
+dtm_train %>% dim()
+
+glmnet_classifier <- dtm_train %>% 
+  cv.glmnet(y = train[['spam']], 
+            family = 'binomial',
+            type.measure = "auc",
+            nfolds = 4,
+            thresh = 0.001,
+            maxit = 1000)
+
+glmnet_classifier$cvm %>% max() %>% round(3) %>% paste("-> Max AUC")
+
+dtm_test <- it_test %>% create_dtm(vectorizer)
+preds <- predict(glmnet_classifier, dtm_test, type = 'response')[,1]
+glmnet:::auc(test$spam, preds) %>% round(2)
+
+
+# N Grams ----
+vocab <- it_train %>% create_vocabulary(ngram = c(1L, 2L))
+
+vocab <- vocab %>% 
+  prune_vocabulary(term_count_min = 10, 
+                   doc_proportion_max = 0.5)
+
+bigram_vectorizer <- vocab %>% vocab_vectorizer()
+
+dtm_train <- it_train %>% create_dtm(bigram_vectorizer)
+dtm_train %>% dim()
+
+glmnet_classifier <- dtm_train %>% 
+  cv.glmnet(y = train[['spam']], 
+            family = 'binomial',
+            type.measure = "auc",
+            nfolds = 4,
+            thresh = 0.001,
+            maxit = 1000)
+
+glmnet_classifier$cvm %>% max() %>% round(3) %>% paste("-> Max AUC")
+
+dtm_test <- it_test %>% create_dtm(bigram_vectorizer)
+preds <- predict(glmnet_classifier, dtm_test, type = 'response')[,1]
+glmnet:::auc(test$spam, preds) %>% round(2)
